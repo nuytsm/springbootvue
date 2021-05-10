b78460b5f7168fbd4990e88f31285fa7682d4061
mnuyts <mnuyts@LK-D47BV32.ION.local>
2021-05-10 09:57:10 +0200

    Extra spaties

--- springboothelloworld/src/main/java/helloWorld/AlbumRepository.java
+++ springboothelloworld/src/main/java/helloWorld/AlbumRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends CrudRepository<Album, Integer> {
+	
+	
+	
 	@Query(value = "Select * from Albums", nativeQuery = true)
 	Iterable<Album> getAllAlbums();
 }
